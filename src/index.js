e0a8157bcc92cefc45e063a9d761cbb68bbb1224
YoProgrammer <yuttasakcom@gmail.com>
2017-08-13 02:56:53 +0700

    Add client

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ require('./routes/billingRoutes')(app)
 
 if (process.env.NODE_ENV === 'production') {
   const path = require('path')
-  app.use(express.static(path.resolve(__dirname, 'client/build')))
+  app.use(express.static('client/build'))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
