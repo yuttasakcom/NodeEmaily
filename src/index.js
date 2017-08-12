2b336221d23d767a11769ea0b6098a4107dd0c58
YoProgrammer <yuttasakcom@gmail.com>
2017-08-13 02:51:37 +0700

    Add client

--- src/index.js
+++ src/index.js
@@ -25,8 +25,8 @@ require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
   const path = require('path')
+  app.use(express.static(path.resolve(__dirname, './client/build')))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
